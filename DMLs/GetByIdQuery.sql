bb5f54a2da16d32b8fc9117cfbe802a47d8dcecb
Trollerolle <tjtr63311@edu.ucl.dk>
2024-09-20 20:35:31 +0200

    gamle query rettet til 2.0

--- DMLs/GetByIdQuery.sql
+++ DMLs/GetByIdQuery.sql
@@ -1,10 +1,10 @@
 SELECT * FROM 
 -- StackOverflow link til table aliases på ADDRESS https://stackoverflow.com/questions/42742944/mysql-join-with-2-foreign-keys-in-same-table-referencing-same-key
-	(SELECT ASSIGNMENTS.RegionAssignmentId, ASSIGNMENTS.AssignmentTypeId, Type, Start, Finish, Description, IsMatched, AmbulanceId, S.Zip AS StartZip, S.RegionId AS StartRegionId, S.Address AS StartAddress, E.Zip AS EndZip, E.RegionId AS EndRegionId, E.Address AS EndAddress
-	FROM (((ASSIGNMENTS_ADRESS 
-	FULL OUTER JOIN ASSIGNMENTS ON ASSIGNMENTS.RegionAssignmentId=ASSIGNMENTS_ADRESS.RegionAssignmentId) 
+	(SELECT ASSIGNMENTS.RegionAssignmentId, ASSIGNMENTS.AssignmentTypeId, Type, Start, Finish, Description, IsMatched, AmbulanceId, S.Zip AS StartZip, S.RegionId AS StartRegionId, S.Road AS StartAddress, E.Zip AS EndZip, E.RegionId AS EndRegionId, E.Road AS EndAddress
+	FROM (((ASSIGNMENTS_ADDRESS 
+	FULL OUTER JOIN ASSIGNMENTS ON ASSIGNMENTS.RegionAssignmentId=ASSIGNMENTS_ADDRESS.RegionAssignmentId) 
 	FULL OUTER JOIN ASSIGNMENT_TYPES ON ASSIGNMENT_TYPES.AssignmentTypeId=ASSIGNMENTS.AssignmentTypeId)
-	FULL OUTER JOIN ADDRESS AS S ON S.AddressId=ASSIGNMENTS_ADRESS.StartAddress
-	FULL OUTER JOIN ADDRESS AS E ON E.AddressId=ASSIGNMENTS_ADRESS.EndAddress)) AS A
+	FULL OUTER JOIN ADDRESS AS S ON S.AddressId=ASSIGNMENTS_ADDRESS.StartAddress
+	FULL OUTER JOIN ADDRESS AS E ON E.AddressId=ASSIGNMENTS_ADDRESS.EndAddress)) AS A
 WHERE A.RegionAssignmentId = '33-CD'
 
