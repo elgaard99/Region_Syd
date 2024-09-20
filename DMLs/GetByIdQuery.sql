4fae92f5f8d9977f32b44aac61f08ee195eec8e9
Trollerolle <tjtr63311@edu.ucl.dk>
2024-09-20 19:26:39 +0200

    Nyt forslag til GetIdQuary

--- DMLs/GetByIdQuery.sql
+++ DMLs/GetByIdQuery.sql
@@ -1,5 +1,13 @@
 SELECT * FROM 
-	(SELECT ASSIGNMENTS.RegionAssignmentId, AssignmentTypeId, _Start, Finish, _Description, IsMatched, AmbulanceId, StartAdress, EndAdress
-	FROM ASSIGNMENTS_ADDRESS FULL OUTER JOIN ASSIGNMENTS ON ASSIGNMENTS.RegionAssignmentId=ASSIGNMENTS_ADDRESS.RegionAssignmentId) AS A
-WHERE A.RegionAssignmentId = '34-CD'
+-- Select mangler Zip AS EndZip, RegionId AS EndRegionId, ADDRESS.Address AS EndAddress
+	(SELECT ASSIGNMENTS.RegionAssignmentId, ASSIGNMENTS.AssignmentTypeId, Type, Start, Finish, Description, IsMatched, AmbulanceId, Zip AS StartZip, RegionId AS StartRegionId, ADDRESS.Address AS StartAddress
+	FROM (((ASSIGNMENTS_ADRESS 
+	FULL OUTER JOIN ASSIGNMENTS ON ASSIGNMENTS.RegionAssignmentId=ASSIGNMENTS_ADRESS.RegionAssignmentId) 
+	FULL OUTER JOIN ASSIGNMENT_TYPES ON ASSIGNMENT_TYPES.AssignmentTypeId=ASSIGNMENTS.AssignmentTypeId)
+	FULL OUTER JOIN ADDRESS ON ADDRESS.AddressId=ASSIGNMENTS_ADRESS.StartAddress)) AS A
+	-- UNION
+	-- (SELECT Zip AS EndZip, RegionId AS EndRegionId, ADDRESS.Address AS EndAddress
+	-- From (A
+	-- FULL OUTER JOIN ADDRESS ON ADDRESS.AddressID=A.EndAdress)) AS B
+WHERE A.RegionAssignmentId = '33-CD'
 
