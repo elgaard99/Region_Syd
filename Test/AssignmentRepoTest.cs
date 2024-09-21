00a67c94b41f648d591afc14bbc0f3e0b1b392aa
Dall <d.scharla@hotmail.com>
2024-09-21 14:41:47 +0200

    uden kommentare

--- Test/AssignmentRepoTest.cs
+++ Test/AssignmentRepoTest.cs
@@ -17,7 +17,7 @@ namespace Test
 
 
         Assignment AssignmentA, AssignmentB, AssignmentC, AssignmentD;
-        AssignmentRepo /*_assignmentRepo,*/ SQLRepo;
+        AssignmentRepo SQLRepo;
         int totalCountOfAssignments;
         //Leger her---------------------------------------------------------------
         public void CountNumberOfRowsInASSIGNMENTSTable() 
@@ -34,51 +34,10 @@ namespace Test
         [TestInitialize]
         public void Init()
         {
-            /*AssignmentA = new Region_Syd.Model.Assignment()
-            {
-                RegionAssignmentId = "A",
-                Start = DateTime.Now,
-                IsMatched = true,
-            };
-            AssignmentB = new Region_Syd.Model.Assignment()
-            {
-                RegionAssignmentId = "B",
-                Start = DateTime.Now.AddHours(1),
-                IsMatched = false,
-                AmbulanceId = "B",
-            };
-            AssignmentC = new Region_Syd.Model.Assignment()
-            {
-                RegionAssignmentId = "C",
-                Start = DateTime.Now.AddHours(5),
-                IsMatched = false,
-                AmbulanceId = "C",
-            };
-            AssignmentD = new Assignment()
-            {
-                RegionAssignmentId = "33-CD",
-                AmbulanceId = "AmCReg2",
-                StartAddress = "Sygehusvej 10, 4000 Roskilde",
-                EndAddress = "Testrupvej 56, 8320 Mårslet",
-                Start = new DateTime(2024, 09, 05, 11, 00, 00),
-                Finish = new DateTime(2014, 09, 05, 13, 30, 00),
-                Description = "Kræver ilt i ambulancen",
-                AssignmentType = AssignmentTypeEnum.C,
-                StartRegion = RegionEnum.RSj,
-                EndRegion = RegionEnum.RM,
-                IsMatched = true
-            };*/
-            //_assignmentRepo = new AssignmentRepo(cs);
-            /*_assignmentRepo.AddToAllAssignments(AssignmentA);
-            _assignmentRepo.AddToAllAssignments(AssignmentB);
-            _assignmentRepo.AddToAllAssignments(AssignmentC);
-
-            _assignmentRepo.AddToAllAssignments(AssignmentA);
-            _assignmentRepo.AddToAllAssignments(AssignmentB);
-            _assignmentRepo.AddToAllAssignments(AssignmentC);*/
+           
 
             SQLRepo = new AssignmentRepo(cs);
-            for (int i = 0; i < 4; i++) //For at være sikker på de første 4 er IsMatched = false, så vi kan teste på disse
+            for (int i = 0; i < 4; i++) 
             {
                 SQLRepo.testAllAssignments[i].IsMatched = false;
             }
@@ -88,16 +47,7 @@ namespace Test
             AssignmentD = SQLRepo.testAllAssignments[3];
             CountNumberOfRowsInASSIGNMENTSTable();
         }
-        /* [TestMethod]
-
-         public void AssignmentsAddedToRepo()
-         {
-             //Assert
-             List<Region_Syd.Model.Assignment> testAssignment = _assignmentRepo.GetAllAssignments();
-             Assert.AreEqual(AssignmentA, testAssignment[3]);
-             Assert.AreEqual(AssignmentB, testAssignment[4]);
-             Assert.AreEqual(AssignmentC, testAssignment[5]);
-         }*/
+        
         [TestMethod]
 
         public void ReAssignAmbulanceTest()
