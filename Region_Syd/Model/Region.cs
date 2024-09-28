94aa4631c9aa43c0c3d8033304a0027d67c12fbe
Christian <csol63295@edu.ucl.dk>
2024-09-28 08:59:16 +0200

    Oprettet Region class og RegionRepo

---
+++ Region_Syd/Model/Region.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Region_Syd.Model
+{
+    class Region
+    {
+		private int _regionId;
+
+		public int RegionId
+		{
+			get { return _regionId; }
+			set { _regionId = value; }
+		}
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		private double _hoursSaved;
+
+		public double HoursSaved
+		{
+			get { return _hoursSaved; }
+			set { _hoursSaved = value; }
+		}
+		private double _distanceSaved;
+
+		public double DistanceSaved
+		{
+			get { return _distanceSaved; }
+			set { _distanceSaved = value; }
+		}
+		public Region (string name, double hoursSaved, double distanceSaved)
+		{
+			Name = name;
+			HoursSaved = hoursSaved;
+			DistanceSaved = distanceSaved;
+		}
+
+
+
+	}
+}
