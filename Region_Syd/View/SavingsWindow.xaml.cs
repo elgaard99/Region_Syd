44897f1b9ee2571b30224e260782f02457abe65e
Trollerolle <tjtr63311@edu.ucl.dk>
2024-09-29 18:57:16 +0200

    ViewModel og view med sampling til Savings

--- Region_Syd/View/SavingsWindow.xaml.cs
+++ Region_Syd/View/SavingsWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Region_Syd.ViewModel;
 
 namespace Region_Syd.View
 {
@@ -22,6 +23,7 @@ namespace Region_Syd.View
 		public SavingsWindow()
 		{
 			InitializeComponent();
+			DataContext = new SavingsViewModel();
 		}
 	}
 }
