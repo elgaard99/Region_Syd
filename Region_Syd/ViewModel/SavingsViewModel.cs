fe3965539dc460547ebc786ffb5447bd4254e3dd
Trollerolle <tjtr63311@edu.ucl.dk>
2024-09-30 09:33:06 +0200

    Savings window Xaml

--- Region_Syd/ViewModel/SavingsViewModel.cs
+++ Region_Syd/ViewModel/SavingsViewModel.cs
@@ -8,39 +8,16 @@ using Region_Syd.Model;
 
 namespace Region_Syd.ViewModel
 {
-    public class Saving
-    {
-        private double _savedHours;
-
-        public double SavedHours
-        {
-            get { return _savedHours; }
-            set { _savedHours = value; }
-        }
-        private RegionEnum _region;
-
-        public RegionEnum Region
-        {
-            get { return _region; }
-            set { _region = value; }
-        }
-
-
-    }
-
+    
     public class SavingsViewModel : ViewModelBase
     {
 
-        public static ObservableCollection<Saving> Savings = new ObservableCollection<Saving>();
+        public static ObservableCollection<Region> Savings = new ObservableCollection<Region>();
 
         public SavingsViewModel()
         {
 
-            Savings.Add(new Saving { Region = RegionEnum.RH, SavedHours = 33.33 });
-            Savings.Add(new Saving { Region = RegionEnum.RM, SavedHours = 323.46 });
-            Savings.Add(new Saving { Region = RegionEnum.RN, SavedHours = 7654.64 });
-            Savings.Add(new Saving { Region = RegionEnum.RSj, SavedHours = 12.46 });
-            Savings.Add(new Saving { Region = RegionEnum.RSy, SavedHours = 982.44 });
+
         }
 
 
